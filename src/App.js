ac9ad4ae7a555892ca6f29220f2086012dd60e5b
Edison Wang <edison763092@gmail.com>
2021-08-27 03:33:59 +0800

    feat: add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import StreamList from './stream/StreamList';
+import Layout from './layout/Layout';
 
 function App() {
   return (
-    <div className="p-4 min-h-screen bg-gray-100">
-      <StreamList />
-    </div>
+    <Layout>
+      <div className="p-4 min-h-screen bg-gray-100">
+        <StreamList />
+      </div>
+    </Layout>
   );
 }
 
---
+++ src/layout/Header.js
@@ -0,0 +1,9 @@
+function Navigation() {
+  return (
+    <header className="h-14 w-full flex items-center bg-gray-400/90 fixed top-0 left-0 z-10 text-xl tracking-wide font-bold backdrop-blur-sm">
+      <div className="pl-10">Holo Viewer</div>
+    </header>
+  );
+}
+
+export default Navigation;
