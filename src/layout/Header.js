69ea857ff34a330074084467de2c13b80fae5054
Edison Wang <edison763092@gmail.com>
2021-08-27 04:45:48 +0800

    refactor(header): replace fixed with sticky

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,6 +1,6 @@
 function Navigation() {
   return (
-    <header className="h-14 w-full flex items-center bg-gray-400/90 fixed top-0 left-0 z-10 text-xl tracking-wide font-bold backdrop-blur-sm">
+    <header className="h-14 w-full flex items-center bg-gray-400/90 sticky top-0 z-10 text-xl tracking-wide font-bold backdrop-blur-sm">
       <div className="pl-10">Holo Viewer</div>
     </header>
   );
