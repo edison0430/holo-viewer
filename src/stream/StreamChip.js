431f1d9e31ead5497347c79e8c4fd36c5002c52d
Edison Wang <edison763092@gmail.com>
2021-08-27 01:46:01 +0800

    fix(stream-item): show duration immediately

--- src/stream/StreamChip.js
+++ src/stream/StreamChip.js
@@ -5,25 +5,30 @@ import { useEffect, useState } from 'react';
 dayjs.extend(relativeTime);
 dayjs.extend(duration);
 
+function getDuration(startAt) {
+  const now = dayjs();
+  const duration = dayjs.duration(now.diff(dayjs(startAt)));
+
+  return duration.format('HH:mm:ss');
+}
+
 function StreamChip({ stream }) {
-  const [startFromNow, setStartFromNow] = useState('');
+  const { start_at: startAt } = stream;
+  const [duration, setDuration] = useState(() => getDuration(startAt));
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      const now = dayjs();
-      const startAt = dayjs(stream.start_at);
-      const duration = dayjs.duration(now.diff(startAt));
-      setStartFromNow(duration.format('HH:mm:ss'));
+      setDuration(getDuration(startAt));
     }, 1000);
 
     return () => {
       clearInterval(intervalId);
     };
-  }, []);
+  }, [startAt]);
 
   return (
     <span className="absolute bottom-2 right-2 text-white font-bold tracking-wide bg-red-600/90 px-2 py-0 rounded-sm">
-      {startFromNow}
+      {duration}
     </span>
   );
 }
